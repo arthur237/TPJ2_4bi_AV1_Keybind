9be7aa4ad928feff03927510cbbafdd2b3ac01be
Arthur Ferreira Cruzal Gomes <arthulek@gmail.com>
2017-12-08 21:44:36 -0200

    AV1_4BI_TPJ2
    Primeira avaliação TPJ2 4 bimestre

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inpu : MonoBehaviour
+{
+    public static KeyCode pulo;
+    public static KeyCode frente;
+    public static KeyCode tras;
+    public static KeyCode agachar;
+
+    public static bool GetPulo()
+    {
+        return Input.GetKey(pulo);
+    }
+    public static bool GetFrente()
+    {
+        return Input.GetKey(frente);
+    }
+    public static bool GetTras()
+    {
+        return Input.GetKey(tras);
+    }
+    public static bool GetAgachar()
+    {
+        return Input.GetKey(agachar);
+    }
+
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public sealed class PlayerJump : MonoBehaviour
+{
+    public PlayerController controller;
+    public KeyCode inputPular;
+
+    public float jumpImpulse = 15.0f;
+
+    private void Start()
+    {
+        Inpu.pulo = inputPular;
+    }
+    private void Update()
+    {
+        if (controller.isGrounded && !controller.isStunned && Inpu.GetPulo())
+            controller.playerRigidbody.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
+    }
+}
